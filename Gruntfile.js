a546ff47f7f967a8db6b1d3efb7ec3b9a0158c56
ajsr <ajsoriar@gmail.com>
2021-07-09 23:13:44 +0200

    Improve grunt workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,43 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    var getDate = (timestamp) => {
+        var dt = null;
+        var str;
+        if (!timestamp) timestamp = Date.now();
+        dt = new Date(timestamp);
+        str = dt.getFullYear() + '-';
+        if (dt.getMonth() < 9) str += '0';
+        str += dt.getMonth() + 1;
+        str += '-';
+        if (dt.getDate() < 10) str += '0';
+        str += dt.getDate();
+        return str;
+    };
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         clean: {
             build: {
-                src: ['dist/*.*','dist/css/*.*','src/css/*.css']
+                src: ['dist/*.*', 'dist/css/*.*', 'src/css/*.css']
             }
         },
         copy: {
             build: {
-                files: [{
-                    cwd: 'src',
-                    src: [
-                        '*.js',
-                        '*.css'
-                    ],
-                    dest: 'dist',
-                    expand: true
-                },{
-                    cwd: 'src/css',
-                    src: [
-                        '*.css'
-                    ],
-                    dest: 'dist/css',
-                    expand: true
-                }]
+                files: [
+                    {
+                        cwd: 'src',
+                        src: ['*.js', '*.css'],
+                        dest: 'dist',
+                        expand: true
+                    },
+                    {
+                        cwd: 'src/css',
+                        src: ['*.css'],
+                        dest: 'dist/css',
+                        expand: true
+                    }
+                ]
             }
         },
         uglify: {
@@ -44,28 +56,108 @@ module.exports = function(grunt) {
         },
         cssmin: {
             target: {
-                files: [{
-                    expand: true,
-                    cwd: 'dist',
-                    src: ['*.css', '!*.min.css'],
-                    dest: 'dist',
-                    ext: '.min.css'
-                }]
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist',
+                        src: ['*.css', '!*.min.css'],
+                        dest: 'dist',
+                        ext: '.min.css'
+                    }
+                ]
+            }
+        },
+        remove_comments: {
+            js: {
+                options: {
+                    multiline: true,
+                    singleline: true,
+                    keepSpecialComments: false
+                },
+                cwd: 'dist',
+                src: 'loading-layer.js',
+                expand: true,
+                dest: 'dist'
+            },
+            css: {
+                options: {
+                    multiline: true,
+                    singleline: true,
+                    keepSpecialComments: false
+                },
+                cwd: 'dist',
+                src: 'loading-layer.css',
+                expand: true,
+                dest: 'dist'
+            }
+        },
+        concat: {
+            js: {
+                files: [
+                    {
+                        src: ['src/header.txt', 'dist/loading-layer.js'],
+                        dest: 'dist/loading-layer.js'
+                    },
+                    {
+                        src: ['src/header.txt', 'dist/loading-layer.min.js'],
+                        dest: 'dist/loading-layer.min.js'
+                    }
+                ]
+            },
+            css: {
+                files: [
+                    {
+                        src: ['src/header.txt', 'dist/loading-layer.css'],
+                        dest: 'dist/loading-layer.css'
+                    },
+                    {
+                        src: ['src/header.txt', 'dist/loading-layer.min.css'],
+                        dest: 'dist/loading-layer.min.css'
+                    }
+                ]
+            }
+        },
+        replace: {
+            header: {
+                options: {
+                    patterns: [
+                        {
+                            json: {
+                                'version-number': '<%= pkg.version %>',
+                                'version-date': getDate(Date.now())
+                            }
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        src: ['dist/loading-layer.js'],
+                        dest: 'dist/loading-layer.js'
+                    },
+                    {
+                        src: ['dist/loading-layer.css'],
+                        dest: 'dist/loading-layer.css'
+                    },
+                    {
+                        src: ['dist/loading-layer.min.js'],
+                        dest: 'dist/loading-layer.min.js'
+                    },
+                    {
+                        src: ['dist/loading-layer.min.css'],
+                        dest: 'dist/loading-layer.min.css'
+                    }
+                ]
             }
         }
-
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-remove-comments');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-
-    grunt.registerTask( 
-        'build',
-        'Compiles all of the assets and files to dist directory.',
-        ['clean', 'copy', 'uglify', 'cssmin']
-    );
-
+    grunt.registerTask('build', 'Compiles all of the assets and files to dist directory.', ['clean', 'copy', 'remove_comments:js', 'remove_comments:css', 'uglify', 'cssmin', 'concat:js', 'concat:css', 'replace:header']);
 };
